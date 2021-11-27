099e594c24256ba379303dd54ef0ce4b0822c0c9
Anna Fernandes <anna.cbf@gmail.com>
2021-11-27 12:22:21 -0300

    Completed challenge tasks.

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,19 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent maxWidth="max-content" backgroundColor="gray.800">
+        <ModalBody p={0}>
+          <Image src={imgUrl} width={900} height={600} />
+        </ModalBody>
+        <ModalFooter justifyContent="start">
+          <Link href={imgUrl} target="_blank">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
